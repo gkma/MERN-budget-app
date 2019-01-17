561d1b53f0bdcd6e4b7842a76f582dda2e1d5011
Garron Ma <garronma@gmail.com>
2019-01-16 20:36:13 -0800

    added setBudget

--- src/client/actions/actionCreators.js
+++ src/client/actions/actionCreators.js
@@ -1,5 +1,10 @@
 import * as actions from './actionTypes';
 
-export const increment = () => ({
-  type: actions.INCREMENT_COUNT
+// export const increment = () => ({
+//   type: actions.INCREMENT_COUNT
+// });
+
+export const setBudget = input => ({
+  type: actions.SET_BUDGET,
+  payload: input
 });
