47a690e9aebdc79460b56de7c4efff100557fd5a
Garron Ma <garronma@gmail.com>
2019-01-16 20:36:52 -0800

    added budgetReducer

--- src/client/reducers/rootReducer.js
+++ src/client/reducers/rootReducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
-import testReducer from './testReducer';
+
+// import testReducer from './testReducer';
+import budgetReducer from './budgetReducer';
 
 const rootReducer = combineReducers({
-  test: testReducer
+  // test: testReducer
+  budget: budgetReducer
 });
 
 export default rootReducer;
