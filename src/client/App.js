a504286f3116e018224ae553ed3ed2bc9d5ffdaf
Garron Ma <garronma@gmail.com>
2019-01-16 16:21:33 -0800

    added boilerplate files

---
+++ src/client/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return <h1>Something special cookin' up here...</h1>;
+  }
+}
+
+export default App;
