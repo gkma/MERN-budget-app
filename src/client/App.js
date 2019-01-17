16f53c0364de159769f559182d1d96049aad9f8e
Garron Ma <garronma@gmail.com>
2019-01-16 17:43:15 -0800

    added incrementer test

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-import './App.css';
+import * as actions from './actions/actionCreators';
+
+import './assets/style/App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <h1>Something special cookin' up here...</h1>;
+    const { count, increment } = this.props;
+    return (
+      <div>
+        <p>hello there</p>
+        <h1>{count}</h1>
+        <button onClick={increment}>+</button>
+      </div>
+    );
   }
 }
 
-export default App;
+const mapStateToProps = (state /*, ownProps*/) => {
+  return {
+    count: state.test.count
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increment: () => {
+      dispatch(actions.increment());
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
